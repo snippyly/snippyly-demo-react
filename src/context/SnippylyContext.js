af5ff0aafd451f5f87917235da3d0f44c0db6358
vivek <vivek@snippyly.com>
2022-04-25 12:31:34 +0530

    Added 'snippyly-comments'. Updated snippyly to 1.0.29.

--- src/context/SnippylyContext.js
+++ src/context/SnippylyContext.js
@@ -1,7 +1,15 @@
 import { createContext, useContext } from 'react';
 
+/**
+ * @typedef {import("@snippyly/sdk").Snippyly} SnippylyClient
+ */
+
 export const SnippylyContext = createContext();
 
+/**
+ * 
+ * @returns {{client: SnippylyClient}}
+ */
 export function useSnippylyClient() {
     return useContext(SnippylyContext);
 }
